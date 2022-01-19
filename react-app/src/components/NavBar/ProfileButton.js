8e343ee64405c3e80bf26cd220f3e2360060ed73
JamesRR91 <jamesrusr@gmail.com>
2022-01-19 15:20:25 -0800

    fixing nav bar

---
+++ react-app/src/components/NavBar/ProfileButton.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import * as sessionActions from '../../store/session';
+import './ProfileButton.css'
+
+function ProfileButton({ user }) {
+    console.log(user)
+    const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
+    const history = useHistory();
+    const openMenu = () => {
+        if (showMenu) return;
+        setShowMenu(true);
+    }
+    useEffect(() => {
+        if (!showMenu) return;
+        const closeMenu = () => {
+            setShowMenu(false);
+        };
+        document.addEventListener('click', closeMenu);
+        return () => document.removeEventListener('click', closeMenu);
+    }, [showMenu]);
+    const logout = (e) => {
+        e.preventDefault();
+        history.push('/');
+        dispatch(sessionActions.logout());
+    }
+    return (
+        <div className='center-profile'>
+          <span className='welcome-message'>{`Welcome, ${user.username}!`}</span>
+          <button onClick={openMenu}>
+            <i className="fas fa-user"></i>
+          </button>
+          {showMenu && (
+            <ul className="profile-dropdown">
+              <li className='profile-list-item'>Username:{user.username}</li>
+              <li className='profile-list-item'>Email:{user.email}</li>
+              <li>
+                <button className='logout-button'onClick={logout}>Log Out</button>
+              </li>
+            </ul>
+          )}
+        </div>
+      );
+}
+export default ProfileButton
