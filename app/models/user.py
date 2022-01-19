250a6f50ce54236d9217560505f7ee77afabf5f6
JamesRR91 <jamesrusr@gmail.com>
2022-01-19 14:57:52 -0800

    finished starting seed, will need to add more later

--- app/models/user.py
+++ app/models/user.py
@@ -10,6 +10,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    products=db.relationship('Product', back_populates='user', cascade='all, delete')
 
     @property
     def password(self):
