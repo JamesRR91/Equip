d2ee6ad883ad362fe75f391494a1d9e040020678
JamesRR91 <jamesrusr@gmail.com>
2022-01-28 10:39:37 -0800

    testing some stuff

--- react-app/src/components/SingleProduct/SingleProduct.js
+++ react-app/src/components/SingleProduct/SingleProduct.js
@@ -1,4 +1,4 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router';
 import { getProducts } from '../../store/products';
@@ -14,8 +14,20 @@ const { id } = useParams()
 
 
 const thisProductObj= useSelector((state) => state.product.inventory);
+const products = Object.values(thisProductObj);
 const thisProductObjId=thisProductObj[id];
 const sessionUserCheck=useSelector((state) => state.session.user);
+const [users, setUsers]= useState([]);
+const seller=users?.find(user => user.id === thisProductObjId.user_id);
+console.log('SESSION', sessionUserCheck)
+console.log('USER', users);
+console.log('SELLER', seller);
+const otherProducts = products.filter(otherProduct => {
+    if(otherProduct.user_id === seller?.id){
+        if(otherProduct.id !== thisProductObjId.id) return otherProduct;
+    }
+})
+
 let sessionLinks;
 if(sessionUserCheck?.id===thisProductObjId?.user_id) {
     sessionLinks = (
@@ -32,6 +44,12 @@ if(sessionUserCheck?.id===thisProductObjId?.user_id) {
 
 useEffect(() => {
     dispatch(getProducts())
+    async function fetchData() {
+        const response = await fetch('/api/users');
+        const data = await response.json();
+        setUsers(data.users);
+    }
+    fetchData();
 }, [dispatch])
 
 return (
@@ -39,6 +57,7 @@ return (
     {sessionUserCheck? <PostReviewModal id={id}/>: null}
     <div className='single-product-container'>
         <div className='single-product-content'>
+        <h3>{seller?.username}</h3>
         <h1>{thisProductObjId?.product_name}</h1>
         <p className='single-product-price'>${thisProductObjId?.product_price.toFixed(2)}</p>
         <p className='single-product-desc'>{thisProductObjId?.product_description}</p>
