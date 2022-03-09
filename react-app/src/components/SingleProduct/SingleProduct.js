32f0de9f2f29f53c176fef8bf648a021296ca23e
JamesRR91 <jamesrusr@gmail.com>
2022-03-09 13:42:25 -0800

    show owner name

--- react-app/src/components/SingleProduct/SingleProduct.js
+++ react-app/src/components/SingleProduct/SingleProduct.js
@@ -1,4 +1,4 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router';
 import { getProducts } from '../../store/products';
@@ -12,14 +12,26 @@ export default function SingleProduct(){
 const dispatch = useDispatch()
 const { id } = useParams()
 
+const [users, setUsers] = useState([]);
 
-const thisProductObj= useSelector((state) => state.product.inventory);
-const thisProductObjId=thisProductObj[id];
-const sessionUserCheck=useSelector((state) => state.session.user);
-let sessionLinks;
-if(sessionUserCheck?.id===thisProductObjId?.user_id) {
-    sessionLinks = (
-        <div className='session-row'>
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('/api/users/');
+      const responseData = await response.json();
+      setUsers(responseData.users);
+    }
+    fetchData();
+  }, []);
+
+  
+  
+  const thisProductObj= useSelector((state) => state.product.inventory);
+  const thisProductObjId=thisProductObj[id];
+  const sessionUserCheck=useSelector((state) => state.session.user);
+  let sessionLinks;
+  if(sessionUserCheck?.id===thisProductObjId?.user_id) {
+      sessionLinks = (
+          <div className='session-row'>
         <ul className='session-row-list'>
         <li className='session-row-item'>
                 <DeleteProductModal id={id} />
@@ -29,7 +41,11 @@ if(sessionUserCheck?.id===thisProductObjId?.user_id) {
         </div>
     )
 }
+console.log('ARE YOU THE FUCKING USERS', users);
+console.log('THE FUCK AM I PULLING DOWN', thisProductObjId);
+const findUser=users.find(user => user.id ===thisProductObjId.user_id)
 
+console.log('IS THIS THE RIGHT GODDAMN USER', findUser);
 useEffect(() => {
     dispatch(getProducts())
 }, [dispatch])
@@ -40,6 +56,7 @@ return (
     <div className='single-product-container'>
         <div className='single-product-content'>
         <h1>{thisProductObjId?.product_name}</h1>
+        <p className='product-owner'>Owned By:{findUser.username}</p>
         <p className='single-product-price'>${thisProductObjId?.product_price.toFixed(2)}</p>
         <p className='single-product-desc'>{thisProductObjId?.product_description}</p>
         <p className='single-product-quan'>In Stock: {thisProductObjId?.product_quantity}</p>
